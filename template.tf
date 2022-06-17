ee9d5a13aafe22e1e8abc56cf9606985ed66417b
itom-qe <79703359+itom-qe@users.noreply.github.com>
2022-06-17 12:58:56 +0530

    Update template.tf

--- template.tf
+++ template.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.0.0"
+    }
+  }
+}
+
 provider "vsphere" {
   user                  = var.user
   password              = var.password
